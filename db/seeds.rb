9f06e7d9578bfc9a5b1f7645f945967463c14117
vrrodgers <trulymissvicky1982@gmail.com>
2018-09-10 19:06:53 -0500

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 
  # Create Posts
  50.times do
- # #1
+ 
    Post.create!(
- # #2
+ 
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
@@ -12,15 +12,19 @@
  posts = Post.all
  
  # Create Comments
- # #3
+
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
- 
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "A unique title", body: "A unique body")
+  puts "#{Post.count}"
+
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+
