c751fee7e5c4b0075cfa5d1dba7663c35aa96f0e
vrrodgers <trulymissvicky1982@gmail.com>
2018-09-10 19:49:11 -0500

    updated post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
+  resources :posts
+
+  get 'about' => 'welcome#about'
+  
   root 'welcome#index'
+  
   get 'welcome/contact'
   get 'welcome/faq'
 end
