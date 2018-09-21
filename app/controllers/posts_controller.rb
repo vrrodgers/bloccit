19635494815b2b95c347e4ab16ecbec9124d0010
vrrodgers <trulymissvicky1982@gmail.com>
2018-09-21 09:40:55 -0500

    refactor topics and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    
     @posts.each_with_index do |post, index|
       if index == 0 || index % 5 == 0
        post.title = "spam" 
@@ -15,16 +15,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
+  
   def create
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+     @post.topic = @topic
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :new
@@ -41,7 +45,8 @@ class PostsController < ApplicationController
  
      if @post.save
        flash[:notice] = "Post was updated."
-       redirect_to @post
+        redirect_to [@post.topic, @post]
+
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -52,7 +57,7 @@ class PostsController < ApplicationController
      @post = Post.find(params[:id])
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to posts_path
+        redirect_to @post.topic
      else
        flash.now[:alert] = "There was an error deleting the post."
        render :show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
   resources :questions
   get 'about' => 'welcome#about'
   
