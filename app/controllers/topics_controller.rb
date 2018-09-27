bc97b2d788e7a54c3b4644718ccd3dbd14b404c2
vrrodgers <trulymissvicky1982@gmail.com>
2018-09-27 09:06:37 -0500

    added topic validations

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,50 +1,45 @@
 class TopicsController < ApplicationController
+  before_action :set_topic,  only: [:show, :edit, :update, :destroy]
+
     def index
       @topics = Topic.all
     end
+
     def show
-     @topic = Topic.find(params[:id])
     end
+
     def new
      @topic = Topic.new
-     @sponsoredpost = SponsoredPost.new
+      @sponsoredpost = SponsoredPost.new
     end
 
     def create
-        @topic = Topic.new
-        @topic.name = params[:topic][:name]
-        @topic.description = params[:topic][:description]
-        @topic.public = params[:topic][:public]
-    
-        if @topic.save
-          redirect_to @topic, notice: "Topic was saved successfully."
-        else
-          flash.now[:alert] = "Error creating topic. Please try again."
-          render :new
-        end
+      @topic = Topic.new(topic_params)
+
+      if @topic.save
+        redirect_to @topic, notice: "Topic was saved successfully."
+      else
+        flash.now[:alert] = "Error creating topic. Please try again."
+        render :new
+      end
     end
     
    def edit
-      @topic = Topic.find(params[:id])
-    end
+   end
 
     def update
-        @topic = Topic.find(params[:id])
-    
-        @topic.name = params[:topic][:name]
-        @topic.description = params[:topic][:description]
-        @topic.public = params[:topic][:public]
-    
+        @topic.update(topic_params)
+
         if @topic.save
             flash[:notice] = "Topic was updated."
-          redirect_to @topic
+          redirect_to  @topic
         else
           flash.now[:alert] = "Error saving topic. Please try again."
           render :edit
         end
     end
+
     def destroy
-     @topic = Topic.find(params[:id])
  
      if @topic.destroy
        flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
@@ -53,5 +48,18 @@ class TopicsController < ApplicationController
        flash.now[:alert] = "There was an error deleting the topic."
        render :show
      end
+   end
+
+   private 
+
+   def set_topic
+      @topic = Topic.find(params[:id])
+   end
+
+   def topic_params
+    params.require(:topic).permit(:name,
+                                  :description,
+                                  :public
+    )
    end
 end
