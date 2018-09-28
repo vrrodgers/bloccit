38a99b2fbd628d095aba587fb0593cc9d9b4dedd
vrrodgers <trulymissvicky1982@gmail.com>
2018-09-27 19:00:29 -0500

    added callback function for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,29 @@
 class User < ApplicationRecord
-   before_save { self.email = email.downcase if email.present? }
-
-    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-    validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-    validates :password, length: { minimum: 6 }, allow_blank: true
- 
-   validates :email,
-            presence: true,
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 254 }
    
-    has_secure_password
+	 before_save { self.email = email.downcase if email.present? }
+	 before_save :format_name
+
+	
+	 validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+	
+	 validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+	 validates :password, length: { minimum: 6 }, allow_blank: true
+	
+	 validates :email,
+	   presence: true,
+	   uniqueness: { case_sensitive: false },
+	   length: { minimum: 3, maximum: 254 }
+      
+       has_secure_password
+
+	 def format_name
+	 	if name
+	 		name_array = []
+	 		name.split.each do |name_part|
+	 			name_array << name_part.capitalize 
+	 		end
+	 	self.name = name_array.join(' ')
+	 	end
+	 end
+
 end
