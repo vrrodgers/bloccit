e3829b6db46d23ea9b14c84c5e2d4678ccd59b5b
vrrodgers <trulymissvicky1982@gmail.com>
2018-10-03 09:31:19 -0500

    created session controller for signin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
 
   resources :questions
